9cd3cb92d6e262df69183f32b870ecf319fd4988
Aymen Djellal <aymendjellal@gmail.com>
2017-05-13 09:31:20 -0400

    On peut Drag :D !

--- God vs sinners 2/Assets/Script/Touched.cs
+++ God vs sinners 2/Assets/Script/Touched.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 public class Touched : MonoBehaviour {
-
-
+    string controllerHand;
 	// Use this for initialization
 	void Start () {
+        if (this.ToString().Contains("left"))
+            controllerHand = "Left";
+        else
+            controllerHand = "Right";
         if (GetComponent<VRTK_ControllerEvents>() == null)
         {
             Debug.LogError("Need to have VRTK_ControllerEvents component attached to the controller");
@@ -14,6 +17,8 @@ public class Touched : MonoBehaviour {
         }
 
         GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(GodKill);
+        GetComponent<VRTK_ControllerEvents>().TouchpadPressed += new ControllerInteractionEventHandler(PickUp);
+        GetComponent<VRTK_ControllerEvents>().TouchpadReleased += new ControllerInteractionEventHandler(Release);
         Debug.Log("I am alive !");
     }
 
@@ -26,7 +31,35 @@ public class Touched : MonoBehaviour {
                 GameObject.Destroy(inch.gameObject);
         }
     }
-
+    private void PickUp(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        GameObject inch = GetComponent<VRTK_InteractTouch>().GetTouchedObject();
+        if (inch != null)
+        {
+            if (inch.tag == "inch")
+            {
+                inch.GetComponent<agentBehaviorTest>().enabled = false;
+                inch.gameObject.transform.parent = this.transform;
+            }
+        }
+    }
+    private void Release(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Transform inch = this.transform.GetChild(i);
+            if (inch != null)
+            {
+                if (inch.gameObject.tag == "inch")
+                {
+                    float y = inch.position.y;
+                    inch.parent = GameObject.FindGameObjectWithTag("Table").transform;
+                    inch.gameObject.GetComponent<agentBehaviorTest>().enabled = true;
+                    inch.position = new Vector3(inch.position.x, y, inch.position.z);
+                }
+            }
+        }
+    }
     // Update is called once per frame
     void Update ()
     {
