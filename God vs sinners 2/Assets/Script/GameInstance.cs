82721a325b0dc49c732adcd1ed8982edaada0611
Yawen <sytta@live.ca>
2017-05-13 18:49:06 -0400

    Primitive menu done

---
+++ God vs sinners 2/Assets/Script/GameInstance.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameInstance : MonoBehaviour
+{
+    public static GameInstance instance = null;
+    public GameObject MenuMusic;
+    public GameObject MainMusic;
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        //MenuMusic.GetComponent<AudioSource>().Play();
+        //MainMusic.GetComponent<AudioSource>().Stop();
+    }
+    public void ToMainGame()
+    {
+        SceneManager.LoadScene("PanicTestSceneVR", LoadSceneMode.Single);
+        //MenuMusic.GetComponent<AudioSource>().Stop();
+        //MainMusic.GetComponent<AudioSource>().Play();
+    }
+    public void ToEndScreen()
+    {
+
+    }
+}
---
+++ God vs sinners 2/Assets/Script/Menu.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    private Color buttonDisabled;
+    private Color buttonEnabled;
+
+    private int[] playerButtonUpDown;
+    
+    public Button buttonPlay;
+    public Button buttonQuit;
+    public string buttonSelected = "Play";
+
+    // Use this for initialization
+    void Start()
+    {
+        buttonDisabled = Color.white;
+        buttonEnabled = Color.green;//new Color(105, 158,45);
+        
+        Button play = buttonPlay.GetComponent<Button>();
+        Button quit = buttonQuit.GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = -Input.GetAxis("Vertical");
+
+        ////// ----- KEYBOARD -----//////
+        if (vertical < 0)
+        {
+            buttonPlay.GetComponent<Image>().color = buttonEnabled;
+            buttonQuit.GetComponent<Image>().color = buttonDisabled;
+            buttonSelected = "Play";
+        }
+        else if (vertical > 0)
+        {
+            buttonPlay.GetComponent<Image>().color = buttonDisabled;
+            buttonQuit.GetComponent<Image>().color = buttonEnabled;
+            buttonSelected = "Quit";
+        }
+
+        if (Input.GetButton("Jump") && buttonSelected == "Play")
+        {
+            GameInstance.instance.ToMainGame();
+        }
+
+        if (Input.GetButton("Jump") && buttonSelected == "Quit")
+        {
+            Application.Quit();
+        }
+        ////// ----- END KEYBOARD -----//////
+
+        //if (raycasting to buttonPlay) {
+        //    buttonPlay.GetComponent<Image>().color = buttonEnabled;
+        //    buttonQuit.GetComponent<Image>().color = buttonDisabled;
+        //    buttonSelected = "Play";
+        //} else if (raycasting to buttonQuit) {
+        //    buttonPlay.GetComponent<Image>().color = buttonDisabled;
+        //    buttonQuit.GetComponent<Image>().color = buttonEnabled;
+        //    buttonSelected = "Quit";
+        //}
+
+        // Start game
+        //if (press && buttonSelected == "Play")
+        //{
+        //    GameInstance.instance.ToMainGame();
+        //}
+
+        // Quit game
+        //if (press && buttonSelected == "Quit")
+        //{
+        //    Application.Quit();
+        //}
+    }
+}
+
