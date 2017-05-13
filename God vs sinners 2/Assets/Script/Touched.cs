a46b1542b51d664e8d6abd38800e6ba1ee93f59a
Aymen Djellal <aymendjellal@gmail.com>
2017-05-13 06:43:22 -0400

    VRTK stuff

---
+++ God vs sinners 2/Assets/Script/Touched.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class Touched : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        if (GetComponent<VRTK_ControllerEvents>() == null)
+        {
+            Debug.LogError("Need to have VRTK_ControllerEvents component attached to the controller");
+            return;
+        }
+        GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(GodKill); ;
+
+        Debug.Log("I am alive !");
+    }
+
+    private void GodKill(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        Debug.Log("Kill me !");
+        GameObject.Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
