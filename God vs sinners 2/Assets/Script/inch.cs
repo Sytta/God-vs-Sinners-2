8bdf2dabb854e69eb6ffd7b32efca4f31ba7988c
Aymen Djellal <aymendjellal@gmail.com>
2017-05-14 14:59:50 -0400

    ANIMATIONNNNNNNNNNNNNNNNNNNNS

--- God vs sinners 2/Assets/Script/inch.cs
+++ God vs sinners 2/Assets/Script/inch.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class inch : MonoBehaviour {
 
+    //public static Vector3 previous = new Vector3(0, 0, 0);
+    private String sAnim;
+
     // Use this for initialization
     void Start () {
-        
+       
     }
 
-    //public static Vector3 previous = new Vector3(0, 0, 0);
 
     // Update is called once per frame
     void Update() {
-        Animator anim = gameObject.GetComponent<Animator>();
+        Animator anim = gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
         if (anim != null)
         {
+            if (String.IsNullOrEmpty(sAnim))
+            { 
+                sAnim = anim.name;
+                sAnim = sAnim.Replace("(Clone)", "");
+            }
             if (gameObject.transform.position.y > 1.01 || gameObject.transform.position.y < 0.99)
             {
                 anim.enabled = false;
@@ -33,7 +41,7 @@ public class inch : MonoBehaviour {
             }
         }
         transform.GetChild(0).transform.position = transform.position;
-
+        setAnmationDefault();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -82,5 +90,95 @@ public class inch : MonoBehaviour {
             GetComponent<Rigidbody>().velocity = collision.relativeVelocity.magnitude  * direction * 2.0f;
             */
         }
+    }
+
+    void setAnmationDefault()
+    {
+
+        Animator animator = gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = Resources.Load(sAnim) as RuntimeAnimatorController;
+    }
+    void setAnmationAttack()
+    {
+        String name;
+        if (sAnim.Contains("billy"))
+        {
+            name = "billyjean_ATTACK";
+        }
+        else if (sAnim.Contains("enrico"))
+        {
+            name = "enrico_attack";
+        }
+        else if (sAnim.Contains("FatJenna"))
+        {
+            name = "FatJenna_ATTACK";
+        }
+        else if (sAnim.Contains("normal_girl"))
+        {
+            name = "normal_girl_ATTACK";
+        }
+        else
+        {
+            name = sAnim;
+        }
+
+        Animator animator = gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = Resources.Load(name) as RuntimeAnimatorController;
+    }
+
+    void setAnmationRun()
+    {
+        String name;
+        if (sAnim.Contains("billy"))
+        {
+            name = "billyjean_RUN";
+        }
+        else if (sAnim.Contains("enrico"))
+        {
+            name = "enrico_RUN";
+        }
+        else if (sAnim.Contains("FatJenna"))
+        {
+            name = "FatJenna_RUN";
+        }
+        else if (sAnim.Contains("normal_girl"))
+        {
+            name = "normal_girl_RUN";
+        }
+        else
+        {
+            name = sAnim;
+        }
+
+        Animator animator = gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = Resources.Load(name) as RuntimeAnimatorController;
+    }
+
+    void setAnmationPraise()
+    {
+        String name;
+        if (sAnim.Contains("billy"))
+        {
+            name = "billyjean_pickmepickme";
+        }
+        else if (sAnim.Contains("enrico"))
+        {
+            name = "enrico_pickmepickme";
+        }
+        else if (sAnim.Contains("FatJenna"))
+        {
+            name = "FatJenna_pickmepickme";
+        }
+        else if (sAnim.Contains("normal_girl"))
+        {
+            name = "normal_girl_pickmepickme";
+        }
+        else
+        {
+            name = sAnim;
+        }
+
+        Animator animator = gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = Resources.Load(name) as RuntimeAnimatorController;
     }
 }
