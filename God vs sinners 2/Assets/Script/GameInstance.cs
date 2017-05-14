2ac6f1c8f81294d15c3c668a1cb5ecd231bcb968
Aymen Djellal <aymendjellal@gmail.com>
2017-05-14 10:13:34 -0400

    Changed main scene to Map

--- God vs sinners 2/Assets/Script/GameInstance.cs
+++ God vs sinners 2/Assets/Script/GameInstance.cs
@@ -38,7 +38,7 @@ public class GameInstance : MonoBehaviour
     }
     public void ToMainGame()
     {
-        SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+        SceneManager.LoadScene("Map", LoadSceneMode.Single);
         //MenuMusic.GetComponent<AudioSource>().Stop();
         //MainMusic.GetComponent<AudioSource>().Play();
     }
