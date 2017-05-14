88987f40898c45226179b1ac9ae36a5f11e6c71a
Aymen Djellal <aymendjellal@gmail.com>
2017-05-14 16:34:18 -0400

    Hack Menu

---
+++ God vs sinners 2/Assets/Script/UITouch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class UITouch : MonoBehaviour
+{
+    string controllerHand;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(GO);
+        //GetComponent<VRTK_ControllerEvents>().TouchpadPressed += new ControllerInteractionEventHandler(GO);
+        //GetComponent<VRTK_ControllerEvents>().TouchpadReleased += new ControllerInteractionEventHandler(GO);
+    }
+
+    private void GO(object sender, VRTK.ControllerInteractionEventArgs e)
+    {
+        if (((GameObject)GameObject.FindGameObjectsWithTag("Menu")[0]).GetComponent<Menu>().buttonSelected == "Play")
+            GameInstance.instance.ToMainGame();
+        else
+            Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
