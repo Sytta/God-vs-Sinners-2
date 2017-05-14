7060bf77b0e4d1ae08884bc25e90a1c612440481
zihui <zihui.zhong@polymtl.ca>
2017-05-14 13:12:07 -0400

    Thought bubbles.

---
+++ God vs sinners 2/Assets/Script/Billboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public float length = 3;
+    void start()
+    {
+
+    }
+    void Update()
+    {
+        length -= UnityEngine.Time.deltaTime;
+        transform.rotation = Camera.main.transform.rotation;
+        transform.Rotate(new Vector3(90, 180, 0));
+        if (length < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
